e5d9c645267ba8ba61e0f714ffed1e8bc3f6c1a6
flo-l <lacknerflo@gmail.com>
2015-07-04 14:24:09 +0200

    lay groundwork for struct support in source analyzer

--- ext/source_analyzer/lib/modules.rs
+++ ext/source_analyzer/lib/modules.rs
@@ -11,16 +11,19 @@ use syntax::ast::Item_::*;
 use syntax::ast::Mod as SyntaxMod;
 
 use super::fn_headers::FnHeader;
+use super::structs::StructDefinition;
 
 pub struct Module {
     name: String,
     fn_headers: Vec<FnHeader>,
+    structs: Vec<StructDefinition>,
     submodules: Vec<Module>,
 }
 
 impl Module {
     pub fn new(name: String, module: &SyntaxMod) -> Module {
         let mut fn_headers = vec![];
+        let mut structs    = vec![];
         let mut submodules = vec![];
 
         let iter = module.items.iter()
@@ -28,13 +31,20 @@ impl Module {
 
         for x in iter {
             match x.node {
-                ItemFn(ref fn_decl,_,_,_,_,_) => fn_headers.push(FnHeader::new(&x.ident, fn_decl)),
-                ItemMod(ref module) => submodules.push(Module::new(super::get_name_from_ident(&x.ident), module)),
+                ItemFn(ref fn_decl,_,_,_,_,_)
+                    => fn_headers.push(FnHeader::new(&x.ident, fn_decl)),
+                ItemStruct(ref struct_def, ref generics) => {
+
+                    if generics.is_parameterized() { panic!("generics are currently unimplemented") }
+                    structs.push(StructDefinition::new(super::get_name_from_ident(&x.ident), struct_def));
+                },
+                ItemMod(ref module)
+                    => submodules.push(Module::new(super::get_name_from_ident(&x.ident), module)),
                 _ => (),
             }
         }
 
-        Module { name: name, fn_headers: fn_headers, submodules: submodules }
+        Module { name: name, fn_headers: fn_headers, structs: structs, submodules: submodules }
     }
 }
 
@@ -44,6 +54,7 @@ impl ToJson for Module {
         let mut j = BTreeMap::new();
         j.insert("name".to_string(),       self.name.to_json());
         j.insert("fn_headers".to_string(), self.fn_headers.to_json());
+        j.insert("structs".to_string(), self.structs.to_json());
         j.insert("submodules".to_string(), self.submodules.to_json());
         j.to_json()
     }
---
+++ ext/source_analyzer/lib/structs.rs
@@ -0,0 +1,46 @@
+use collections::BTreeMap;
+
+//JSON support
+use serialize::json::{
+    self, ToJson, Json};
+
+// syntax elements
+use syntax::ast::StructDef;
+use syntax::ast::StructFieldKind::*;
+
+pub struct StructDefinition {
+    name: String,
+    //          (name  ,type  )
+    fields: Vec<(String, String)>,
+}
+
+impl StructDefinition {
+    pub fn new(name: String, struct_def: &StructDef) -> StructDefinition {
+        let fields: Vec<(String,String)> = struct_def.fields
+        .iter()
+        .map(|field|
+            match field.node.kind {
+                // TODO: handle visibility
+                NamedField(ref ident, ref _visibility) => {
+                    (super::get_name_from_ident(ident), super::read_type(&field.node.ty.node))
+                },
+                UnnamedField(_) => panic!("tuple structs are not yet implemented"),
+            }
+        )
+        .collect();
+
+        StructDefinition { name: name, fields: fields }
+    }
+}
+
+// for serialization
+// eg: struct Test { a: i64, b: String }
+// =>  {"name":"Test","fields":[["a","i64"],["b","String"]]}
+impl ToJson for StructDefinition {
+    fn to_json(&self) -> json::Json {
+        let mut j = BTreeMap::new();
+        j.insert("name".to_string(), self.name.to_json());
+        j.insert("fields".to_string(), self.fields.to_json());
+        j.to_json()
+    }
+}
